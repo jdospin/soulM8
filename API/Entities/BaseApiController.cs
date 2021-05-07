134b7812a0e94a7a8e8688e484876f458c97d11c
David Ospina <jdospin@hotmail.com>
2021-05-07 00:10:30 -0400

    Add base controller class

---
+++ API/Entities/BaseApiController.cs
@@ -0,0 +1,12 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Entities
+{
+    [ApiController]
+    [Route("api[controller")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
