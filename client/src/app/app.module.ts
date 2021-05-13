dcc2f26259d7b857e7126a98b9d2b7a4a041f96f
David Ospina <jdospin@hotmail.com>
2021-05-12 23:04:00 -0400

    Fix bootstrap dependency. Add navbar to main page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
